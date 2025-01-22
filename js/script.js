15f07a7c6d6d669969ee54abc7089f18d7c1100e
Robin Linacre <robinlinacre@hotmail.com>
2025-01-22 11:25:56 +0000

    fix

--- js/script.js
+++ js/script.js
@@ -429,7 +429,7 @@ function dumpGraphData() {
     source_dataset_l: link.source.sourceDataset,
     unique_id_r: link.target.label,
     source_dataset_r: link.target.sourceDataset,
-    probability: link.probability
+    match_probability: link.probability
   }));
 
   const graphData = {
